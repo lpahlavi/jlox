eb8909ef9c8baf69102b3ef46f7acb7cae12b05e
Louis Pahlavi <louis.pahlavi@mail.utoronto.ca>
2023-06-10 18:36:12 +0200

    Support initializing and evaluating variables

---
+++ src/main/java/com/craftinginterpreters/lox/Environment.java
@@ -0,0 +1,20 @@
+package com.craftinginterpreters.lox;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class Environment {
+    private final Map<String, Object> values = new HashMap<>();
+
+    public Object get (Token name) {
+        if (values.containsKey(name.lexeme)) {
+            return values.get(name.lexeme);
+        }
+        throw new RuntimeError(name, String.format("Undefined variable '%s'.", name.lexeme));
+    }
+
+    public void define (String name, Object value) {
+        // Note: this allows variable re-definition since we do not check for existence!
+        values.put(name, value);
+    }
+}
